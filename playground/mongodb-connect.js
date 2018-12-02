43346380a9be5c787fb5e5e22d8ccb49e94e3c29
CHATHURA SAKUN ELLAWALA <sakun@villvay.com>
2018-12-02 17:58:42 +0530

    init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,20 @@
+const MongoClient = require('mongodb').MongoClient;
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err)
+        return console.log('Unable to connect to mongodb server');
+
+    console.log('Connected to mongodb server');
+
+    db.collection('Todos').insertOne({
+        text: 'something to',
+        completed: false
+    }, (err, result) => {
+        if (err) {
+            return console.log('unable to insert', err);
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2));
+    });
+    db.close();
+});
